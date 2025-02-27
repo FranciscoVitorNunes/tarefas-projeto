2f62b9fdd05b6208a22e5ddf6d924e068506ec35
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-02-27 16:47:08 -0300

    Simples sistema de CRUD de tarefas.

---
+++ README.md
@@ -0,0 +1,44 @@
+📌 Nome do Projeto
+md
+Copiar
+Editar
+# Gerenciador de Tarefas
+Um sistema simples de gerenciamento de tarefas feito com Node.js.
+🛠 Tecnologias Usadas
+md
+Copiar
+Editar
+- Node.js
+- Express.js
+- MongoDB (ou outro banco, se estiver usando)
+- HTML, CSS e JavaScript
+🚀 Como Rodar o Projeto
+md
+Copiar
+Editar
+1. Clone este repositório:
+git clone https://github.com/seu-usuario/nome-do-repo.git
+
+arduino
+Copiar
+Editar
+2. Acesse a pasta do projeto:
+cd nome-do-repo
+
+csharp
+Copiar
+Editar
+3. Instale as dependências:
+npm install
+
+markdown
+Copiar
+Editar
+4. Inicie o servidor:
+node server.js
+
+less
+Copiar
+Editar
+
+O servidor será iniciado em `http://localhost:3000`.
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser');
+
+const app = express();
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.engine('html',require('ejs').renderFile );
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+var tarefas = ['arrumar quarto','lavar louça']
+
+app.get('/',(req, res)=>{
+    res.render('index',{ListTarefas: tarefas});
+});
+
+app.get('/deletar/:id', (req,res)=>{
+    tarefas = tarefas.filter(function(val, index){
+        if(index != req.params.id){
+            return val;
+        }
+    });
+    res.render('index',{ListTarefas: tarefas});
+});
+
+app.post('/', (req,res)=>{
+    tarefas.push(req.body.tarefa);
+
+    res.render('index',{ListTarefas: tarefas});
+});
+
+app.listen(5000,()=>{
+    console.log("Server online...");
+});
